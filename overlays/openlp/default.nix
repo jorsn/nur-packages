0a5216e6a29cbaa7493ce0f30563a04141820dec
Johannes Rosenberger <johannes.rosenberger@jorsn.eu>
2018-10-08 16:23:36 +0200

    added overlay: openlp (pkgs: openlp, tempita-5.3)

---
+++ overlays/openlp/default.nix
@@ -0,0 +1,10 @@
+self: super:
+
+{
+  python3Packages = super.python3Packages // {
+    sqlalchemy_migrate = super.python3Packages.sqlalchemy_migrate.override {
+      tempita = super.python3Packages.callPackage pkgs/python/tempita-53.nix {};
+    };
+  };
+  openlp = super.libsForQt5.callPackage pkgs/openlp {};
+}
